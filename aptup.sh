f253c3c7868dde1e8abeb070a7f0ffdd895d0aac
Valerio Capello <valerio@geody.com>
2022-12-18 19:45:33 +0100

    Add files via upload
    aptup.sh
    - Check for removable packages
    - Fixed compatibility with system languages other than English.

--- aptup.sh
+++ aptup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # aptup
-xver='r2022-08-24 fr2021-08-14';
+xver='r2022-12-18 fr2021-08-14';
 # by Valerio Capello - http://labs.geody.com/ - License: GPL v3.0
 
 
@@ -13,31 +13,45 @@ shwspackcnt=true; # Show installed packages count
 shwspacklast=true; # Show last installed packages
 shwspackmax=5; # Last installed packages output limit
 checkbroken=true; # Check for incorrect installations and broken dependencies
+checkremov=true; # Check for removable packages
 
 
 # Main
 
 echo "AptUp by Valerio Capello - http://labs.geody.com/ - $xver";
 if ( $shwdate ); then
-echo; echo -n "Date: "; date "+%a %d %b %Y %H:%M:%S %Z (UTC%:z)";
+echo; echo -n "Date: ";
+date "+%a %d %b %Y %H:%M:%S %Z (UTC%:z)";
 fi
 if ( $shwspackcnt ); then
-echo; echo -n "Installed packages: "; dpkg --get-selections | wc -l;
+echo; echo -n "Installed packages: ";
+dpkg --get-selections | wc -l;
 fi
 if ( $shwspacklast ); then
-echo; echo "Last installed packages:"; grep " status installed " /var/log/dpkg.log | tail -$shwspackmax;
+echo; echo "Last installed packages:";
+grep " status installed " /var/log/dpkg.log | tail -$shwspackmax;
 fi
 if ( $checkbroken ); then
-echo; echo "Check for incorrect installations and broken dependencies:"; dpkg --audit ; apt-get check ;
+echo; echo "Check for incorrect installations and broken dependencies:";
+dpkg --audit ; apt-get check ;
+fi
+if ( $checkremov ); then
+echo; echo "Check for removable packages:";
+apt-get --assume-no autoremove | head -n -1 ;
+# apt-get -s autoremove | head -n -1
 fi
 if ( $shwsrclist ); then
-echo; echo "Package sources list:"; cat /etc/apt/sources.list ; # | grep 'deb \|deb-src ' ;
+echo; echo "Package sources list:";
+cat /etc/apt/sources.list ; # | grep 'deb \|deb-src ' ;
 fi
-echo; echo "Dowloading package information:"; apt update ;
+echo; echo "Dowloading package information:";
+apt update ;
 upacks="$( apt-get -s upgrade | tail --lines=1 ; )";
-if [[ "$upacks" != "0 upgraded, 0 newly installed, 0 to remove and 0 not upgraded." ]]; then
-echo; echo "Updated packages:"; apt list --upgradable ;
-echo; echo "Simulated Upgrade:"; apt -s upgrade ;
+if [[ "${upacks:0:2}" != "0 " ]]; then
+echo; echo "Updated packages:";
+apt list --upgradable ;
+echo; echo "Simulated Upgrade:";
+apt -s upgrade ;
 else
 echo $upacks ;
 fi
